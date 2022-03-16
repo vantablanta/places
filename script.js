989e2f4c0c0e487c839234799456ec854850357e
vanta blanta <vantablanta@gmail.com>
2022-03-16 17:21:25 +0300

    adds a constructor, creates an instance of constructor, links to Dom inputs, displays dom inputs as cards

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
 // business logic
-let Places = function(){
-    
+let Places = function(place, date, location, landmarks, notes){
+    this.place = place
+    this.date = date
+    this.location = location 
+    this.landmarks = landmarks
+    this.notes = notes
 }
+
+
+// user interface logic
+$(document).ready(function(){
+    $("#form").submit(function(e){
+        e.preventDefault();
+        let place = $("#inputPlace4").val()
+        let date = $("#inputDate4").val()
+        let location = $("#inputLocation").val()
+        let landmarks = $("#inputLandmarks").val()
+        let notes = $("#inputNotes").val()
+
+        let placeVisited = new Places(place, date, location, landmarks, notes)
+        
+        if (placeVisited.place != ""){
+            $("#placeName").append(`<p> ${placeVisited.place} </p>`)
+            $("#placeName").addClass("card-header text-white bg-success mb-3")
+
+        }else{
+            $("#placeName").removeClass("ccard-header text-white bg-success mb-3")
+        }
+
+
+    
+
+
+
+
+
+    })
+
+
+
+
+
+
+})
